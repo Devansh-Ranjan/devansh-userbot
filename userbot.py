36167f02521e65274e35ea0b7f6a5a11d3531f46
DEVANSH RANJAN <ranjandevansh14@gmail.com>
2025-04-11 18:41:05 +0530

    Update userbot.py

--- userbot.py
+++ userbot.py
@@ -17,25 +17,69 @@ load_dotenv()
 
 #Session and API credentials from environment
 
-API_ID = int(os.getenv("API_ID")) API_HASH = os.getenv("API_HASH") SESSION = os.getenv("SESSION")
+API_ID = int(os.getenv("API_ID")) 
+API_HASH = os.getenv("API_HASH") 
+SESSION = os.getenv("SESSION")
 
 bot = TelegramClient(StringSession(SESSION), API_ID, API_HASH)
 
 #Globals
 
-alive_pic = None is_afk = False afk_reason = "I am AFK."
+alive_pic = None 
+is_afk = False 
+afk_reason = "I am AFK."
 
-@bot.on(events.NewMessage(pattern=r"^.alive$")) async def alive(event): pic = alive_pic if alive_pic else "https://telegra.ph/file/e4d9e82a3e9f758a62ff2.jpg" await bot.send_file( event.chat_id, file=pic, caption=f"Userbot is alive!\nOwner: @my_intrusive_thought_won", buttons=[[Button.inline("Commands", data=b"cmds")]], reply_to=event.id )
+@bot.on(events.NewMessage(pattern=r"^.alive$")) 
+async def alive(event): 
+       pic = alive_pic if alive_pic else "https://telegra.ph/file/e4d9e82a3e9f758a62ff2.jpg" 
+       await bot.send_file( 
+         event.chat_id, 
+         file=pic, 
+         caption=f"Userbot is alive!\nOwner: @my_intrusive_thought_won", buttons=[[Button.inline("Commands", data=b"cmds")]], reply_to=event.id )
 
-@bot.on(events.CallbackQuery(data=b"cmds")) async def command_buttons(event): await event.edit( "COMMAND MENU\nTap buttons to see categories:", buttons=[ [Button.inline("Fun", b"fun"), Button.inline("Utility", b"util")], [Button.inline("AFK", b"afk"), Button.inline("Alive", b"alive")], [Button.inline("Back", b"back")] ] )
+@bot.on(events.CallbackQuery(data=b"cmds")) 
+async def command_buttons(event): 
+          await event.edit( 
+            "COMMAND MENU\nTap buttons to see categories:", 
+            buttons=[ 
+              [Button.inline("Fun", b"fun"), Button.inline("Utility", b"util")], 
+              [Button.inline("AFK", b"afk"), Button.inline("Alive", b"alive")], 
+              [Button.inline("Back", b"back")] ] )
 
-@bot.on(events.CallbackQuery(data=b"fun")) async def fun_cmds(event): await event.edit( "Fun Commands\n.afk, .lol, .cry, .tableflip, .hack, .type, .joke", buttons=[Button.inline("Back", b"cmds")] )
+@bot.on(events.CallbackQuery(data=b"fun")) 
+async def fun_cmds(event): 
+           await event.edit( 
+             "Fun Commands\n.afk, .lol, .cry, .tableflip, .hack, .type, .joke", 
+             buttons=[Button.inline("Back", b"cmds")] )
 
-@bot.on(events.CallbackQuery(data=b"util")) async def util_cmds(event): await event.edit( "Utility Commands\n.id, .ping, .time, .weather <city>, .ss <url>", buttons=[Button.inline("Back", b"cmds")] )
+@bot.on(events.CallbackQuery(data=b"util")) 
+async def util_cmds(event): 
+  await event.edit( 
+    "Utility Commands\n.id, .ping, .time, .weather <city>, .ss <url>", 
+    buttons=[Button.inline("Back", b"cmds")] )
 
-@bot.on(events.NewMessage(pattern=r"^.setalive$")) async def set_alive(event): if event.reply_to_msg_id: reply = await event.get_reply_message() if reply.photo: path = await bot.download_media(reply) global alive_pic alive_pic = path await event.reply("Alive picture updated!") else: await event.reply("Reply to a photo to set it as alive.") else: await event.reply("Reply to a photo to set it as alive.")
+@bot.on(events.NewMessage(pattern=r"^.setalive$")) 
+async def set_alive(event): 
+   if event.reply_to_msg_id: 
+     reply = await event.get_reply_message() 
+     if reply.photo: 
+       path = await bot.download_media(reply) 
+       global alive_pic 
+       alive_pic = path 
+  await event.reply("Alive picture updated!") 
+else: 
+   await event.reply("Reply to a photo to set it as alive.") 
+else: 
+  await event.reply("Reply to a photo to set it as alive.")
 
-@bot.on(events.NewMessage(pattern=r"^.afk(?:\s+(.*))?$")) async def set_afk(event): global is_afk, afk_reason is_afk = True reason = event.pattern_match.group(1) if reason: afk_reason = reason await event.reply(f"AFK activated. Reason: {afk_reason}")
+@bot.on(events.NewMessage(pattern=r"^.afk(?:\s+(.*))?$")) 
+async def set_afk(event): 
+  global is_afk, afk_reason 
+  is_afk = True 
+  reason = event.pattern_match.group(1) 
+  if reason: 
+    afk_reason = reason 
+    await event.reply(f"AFK activated. Reason: {afk_reason}")
 
 @bot.on(events.NewMessage()) async def auto_afk_reply(event): if is_afk and event.mentioned: await event.reply(afk_reason)
 
